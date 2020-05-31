b0e45d179d721a00f72657b8a9afdf2afbff5e48
GangaChatrvedi <gangachaturvedi24@gmail.com>
2020-05-31 12:31:47 +0530

    create config to setup keycloak

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,45 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  KeycloakConnectModule,
+  ResourceGuard,
+  AuthGuard,
+} from './keycloak-connect.module';
+import { APP_GUARD } from '@nestjs/core';
+
 
 @Module({
-  imports: [],
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: 'http://localhost:8080/auth',
+      realm: 'master',
+      clientId: 'google-client',
+      secret: 'e5d608e0-ac33-4d4a-9c24-6c94d9db4458'
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // These are in order, see https://docs.nestjs.com/guards#binding-guards
+    // for more information
+
+    AppService, 
+    // This adds a global level authentication guard, you can also have it scoped
+    // if you like.
+    //
+    // Will return a 401 unauthorized when it is unable to
+    // verify the JWT token or Bearer header is missing.
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // This adds a global level resource guard, which is permissive.
+    // Only controllers annotated with @Resource and methods with @Scopes
+    // are handled by this guard.
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    }
+  ],
 })
 export class AppModule {}
---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,59 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Keycloak } from 'keycloak-connect';
+import { KEYCLOAK_INSTANCE, KEYCLOAK_CONNECT_OPTIONS } from '../constants';
+import { KeycloakConnectOptions } from 'src/interface/keycloak-connect-options.interface';
+
+/**
+ * An authentication guard. Will return a 401 unauthorized when it is unable to
+ * verify the JWT token or Bearer header is missing.
+ */
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    @Inject(KEYCLOAK_INSTANCE)
+    private keycloak: Keycloak,
+    @Inject(KEYCLOAK_CONNECT_OPTIONS)
+    private keycloakOpts: KeycloakConnectOptions,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const jwt =
+      this.extractJwtFromCookie(request.cookies) ??
+      this.extractJwt(request.headers);
+    const result = await this.keycloak.grantManager.validateAccessToken(jwt);
+
+    if (typeof result === 'string') {
+      // Attach user info object
+      request.user = await this.keycloak.grantManager.userInfo(jwt);
+      return true;
+    }
+
+    throw new UnauthorizedException();
+  }
+
+  extractJwt(headers: { [key: string]: string }) {
+    if (!headers.authorization) {
+      throw new UnauthorizedException();
+    }
+
+    const auth = headers.authorization.split(' ');
+
+    // We only allow bearer
+    if (auth[0].toLowerCase() !== 'bearer') {
+      throw new UnauthorizedException();
+    }
+
+    return auth[1];
+  }
+
+  extractJwtFromCookie(cookies: { [key: string]: string }) {
+    return cookies[this.keycloakOpts.cookieKey] || cookies.KEYCLOAK_JWT;
+  }
+}
